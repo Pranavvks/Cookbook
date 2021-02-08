49099c630c6796b8588af2313689ec2a62ed93b6
Pranavvks <pranav.vks@gmail.com>
2021-02-08 22:36:42 +0530

    Second Commit

--- cookingbook/ingredients/models.py
+++ cookingbook/ingredients/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class Person(models.Model):
-    name=models.CharField(max_length=50)
-    details=models.CharField(max_length=100)
-    content= models.TextField()
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="ingredients", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- cookingbook/ingredients/schema.py
+++ cookingbook/ingredients/schema.py
@@ -1,17 +1,28 @@
-import graphene 
+import graphene
 from graphene_django import DjangoObjectType
-from .models import Person
 
-class PersonType(DjangoObjectType):
+from .models import Category, Ingredient
+
+class CategoryType(DjangoObjectType):
     class Meta:
-        model = Person
-        fields = ('name','details','content')
+        model = Category
+        fields = ("id", "name", "ingredients")
 
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        fields = ("id", "name", "notes", "category")
 
-class Query (graphene.ObjectType):
-    Person = graphene.Field(PersonType,name=graphene.String())
+class Query(graphene.ObjectType):
+    all_ingredients = graphene.List(IngredientType)
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
 
-    def resolve_Person(self,info,**kwargs):
-        name = kwargs.get('name')
-        return Person.objects.get(name=name)
-        
+    def resolve_all_ingredients(root, info):
+        # We can easily optimize query count in the resolve method
+        return Ingredient.objects.select_related("category").all()
+
+    def resolve_category_by_name(root, info, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
